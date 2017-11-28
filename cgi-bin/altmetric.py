afd4cff82696c506ab0fdf747e20c49d046fd40d
intensifier <christopher.kullenberg@gmail.com>
2017-11-28 16:11:44 +0100

    added altmetric.py

---
+++ cgi-bin/altmetric.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import cgi
+import cgitb
+import sys
+import re
+import os
+import pandas as pd
+#import networkx as nx
+from werkzeug import secure_filename
+import time
+from printhtml import printaltmetric
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+form = cgi.FieldStorage()
+
+
+def openfile():
+    # Get filename here.
+    fileitem = form['filename']
+    fileitem2 = form['filename2']
+    fn = secure_filename(fileitem.filename)
+    fn2 = secure_filename(fileitem2.filename)
+    # Test if the file was uploaded
+    try:
+        if fileitem.filename:
+            open('upload/' + fn, 'wb').write(fileitem.file.read())
+            fn = os.path.basename(fileitem.filename.replace(' ', '-'))
+            #fn = secure_filename(fn)
+            df = pd.read_csv('upload/' + fn)
+            printaltmetric()
+            #print("Antal rader i filen: " + str(len(df)))
+            G = nx.DiGraph()
+            for tweet in df.iterrows():
+                if tweet[1][2].startswith("RT"):
+                    #print("source: " + tweet[1][1] + "<br>")
+
+                    match = re.findall("(?<=RT\s\@).*?(?=\:)", tweet[1][2], re.IGNORECASE)
+                    if match:
+                        #print(match[0] + "<br>---<br>")
+                        G.add_edge(tweet[1][1], match[0])
+            filename = str(time.time())
+            nx.write_gexf(G, "/home/chrisk/digitalametoder.science/results/" + filename + ".gexf")
+            print('''Nätverksfilen för RT-network kan laddas ned <a href="http://digitalametoder.science/results/''' +
+                  filename + '''.gexf">här</a> (högerklicka och välj "spara som")  och sedan öppnas med Gephi.''')
+        elif fileitem2.filename:
+            open('upload/' + fn2, 'wb').write(fileitem2.file.read())
+            fn2 = os.path.basename(fileitem2.filename.replace(' ', '-'))
+            df = pd.read_csv('upload/' + fn2)
+            printaltmetric()
+            G = nx.DiGraph()
+            for tweet in df.iterrows():
+                #print("source: " + str(tweet[1][3]))
+                if tweet[1][6].startswith("RT"):
+                    #print("target: " + tweet[1][6] + "<br>")
+                    match = re.findall("(?<=RT\s\@).*?(?=\:)", tweet[1][6], re.IGNORECASE)
+                    if match:
+                        #print(match[0] + "<br>---<br>")
+                        G.add_edge(tweet[1][3], match[0])
+            filename = str(time.time())
+            nx.write_gexf(G, "/home/chrisk/digitalametoder.science/results/" + filename + ".gexf")
+            print('''Nätverksfilen för RT-network kan laddas ned <a href="http://digitalametoder.science/results/''' +
+                  filename + '''.gexf">här</a> (högerklicka och välj "spara som")  och sedan öppnas med Gephi.''')
+        else:
+            printaltmetric()
+            print('<p>Ingen fil valdes.</p>')
+    except UnicodeError:
+        print("<p>Filen har inte korrekt teckenkodning. Testa att spara om med Unicode / UTF-8.</p>")
+
+
+try:
+    openfile()
+except KeyError:
+    printaltmetric()
+    sys.exit()
+
+print("</body></html>")
--- cgi-bin/printhtml.py
+++ cgi-bin/printhtml.py
@@ -156,3 +156,56 @@ def printtimemessages():
      </main>
    </div>
     ''')
+
+
+def printaltmetric():
+    print("content-type:text/html; charset=utf-8\r\n\r\n")
+    print()
+    print('''
+        <!DOCTYPE html>
+        <html lang="sv">
+
+          <head>
+
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
+
+            <title>Digitala metoder</title>
+
+                <!-- Bootstrap core CSS -->
+                <link href="http://digitalametoder.science/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+                <!-- Custom styles for this template -->
+                <link href="http://digitalametoder.science/css/3-col-portfolio.css" rel="stylesheet">
+
+              </head>
+
+              <body>
+    <div id="container">
+     <main>
+        <h1>Analyzing altmetric scores </h1>
+	<p><b>OBS: filnamnen får inte innehålla specialtecken</b>.<br> Godkänt filnamn är ex. <code>fil.csv</code>.
+    <br>Välj gärna ett unikt och specifikt filnamn, ex. <code>christophersdata.csv</code></p>
+        <h3>A. Web of Science</h3>
+        <p>Exportera till csv-fil i Google Calc/TAGS och ladda upp:</p>
+        <form enctype="multipart/form-data"
+                         action="altmetric.py" method="post">
+         <br><input type="file" name="filename" />
+         <input type="submit" value="Ladda upp" />
+
+        <br><br>
+
+        <h3>B. Scopus</h3>
+        <p>Om du har en csv-fil som samlats in med Netvizz, ladda upp här:</p>
+        <form enctype="multipart/form-data"
+                         action="altmetric.py" method="post">
+         <br><input type="file" name="filename2" />
+         <input type="submit" value="Ladda upp" />
+
+        <br>
+        </section>
+     </main>
+   </div>
+    ''')
