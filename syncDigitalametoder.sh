0b48291571271baf2ec94be7de3704ca903d18f4
Christopher Kullenberg <christopher.kullenberg@gmail.com>
2018-11-18 17:20:19 +0100

    fixed sync script

--- syncDigitalametoder.sh
+++ syncDigitalametoder.sh
@@ -7,4 +7,8 @@ echo "Rsyncing directory, please wait..."
 
 rsync -vrP -e ssh output/ chrisk@genuskollen.se:/home/chrisk/digitalametoder.science/
 
+echo "Then syncing the root folder and sub-directories"
+
+rsync -vrP -e ssh . chrisk@genuskollen.se:/home/chrisk/digitalametoder.science/
+
 echo "Done! Thank you...."
